286e879f1c32ef3a9acb08c2b4791653198b9455
miguelcoria94 <miguelcoria94@gmail.com>
2023-06-13 12:15:29 -0500

    =added dummy articles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,7 +18,7 @@
       </select>
     </div>
     <div class="articles">
-      <div v-for="article in articles" :key="article.title" class="article">
+      <div v-for="article in filteredArticles" :key="article.title" class="article">
         <router-link
           :to="{ name: 'article', params: { article: article } }"
           tag="div"
@@ -78,7 +78,6 @@
 
 <script>
   import TitleSection from '@/components/TitleSection.vue'
-  import axios from 'axios'
 
   export default {
     name: 'Home',
@@ -93,38 +92,140 @@
           'assault',
           'other',
         ],
-        articles: [],
+        articles: [
+          {
+            event_classification: 'mass casualty',
+            sentiment: 'negative',
+            platform: 'The New York Times',
+            title: 'Mass Casualty Event Unfolds in Central Park',
+            body: 'A sudden, violent explosion in Central Park has resulted in a mass casualty event...',
+          },
+          {
+            event_classification: 'natural disaster',
+            sentiment: 'neutral',
+            platform: 'CNN',
+            title: 'Hurricane Season Starts Early, Coastal Cities Prepare',
+            body: 'An early start to the hurricane season has coastal cities bracing for impact...',
+          },
+          {
+            event_classification: 'homicide',
+            sentiment: 'negative',
+            platform: 'The Washington Post',
+            title:
+              'Investigation Underway as City Experiences Spike in Homicides',
+            body: 'The city is under crisis as the number of homicides spike to unprecedented levels...',
+          },
+          {
+            event_classification: 'other',
+            sentiment: 'positive',
+            platform: 'BBC News',
+            title:
+              'Revolutionary Mental Health Treatment Shows Promising Results',
+            body: 'A new treatment for mental health disorders could be a game-changer in the field...',
+          },
+          {
+            event_classification: 'assault',
+            sentiment: 'negative',
+            platform: 'The Guardian',
+            title: 'Rise in Assault Cases Leads to Calls for Stricter Laws',
+            body: 'Following a recent surge in assault cases, citizens are demanding stricter laws...',
+          },
+          {
+            event_classification: 'mass casualty',
+            sentiment: 'negative',
+            platform: 'The New York Times',
+            title: 'Mass Casualty Event Unfolds in Central Park',
+            body: 'A sudden, violent explosion in Central Park has resulted in a mass casualty event. Authorities are on the scene and emergency services are being deployed. The surrounding area has been evacuated and a lockdown is in effect. The cause of the explosion is still under investigation.',
+          },
+          {
+            event_classification: 'natural disaster',
+            sentiment: 'neutral',
+            platform: 'CNN',
+            title: 'Hurricane Season Starts Early, Coastal Cities Prepare',
+            body: 'An early start to the hurricane season has coastal cities bracing for impact. Municipalities are implementing disaster preparedness measures and advising residents to do the same. Experts predict this season to be above average in terms of storm activity. Residents are urged to stay informed and prepared.',
+          },
+          {
+            event_classification: 'homicide',
+            sentiment: 'negative',
+            platform: 'The Washington Post',
+            title:
+              'Investigation Underway as City Experiences Spike in Homicides',
+            body: 'The city is under crisis as the number of homicides spike to unprecedented levels. Law enforcement agencies are struggling to keep up with the surge in violent crime. A special task force has been assembled to investigate the cause behind this sudden increase. The public is asked to remain vigilant and report suspicious activities.',
+          },
+          {
+            event_classification: 'other',
+            sentiment: 'positive',
+            platform: 'BBC News',
+            title:
+              'Revolutionary Mental Health Treatment Shows Promising Results',
+            body: 'A new treatment for mental health disorders could be a game-changer in the field. Early results show significant improvement in patients suffering from depression and anxiety. The treatment involves a combination of cognitive behavioral therapy and new, non-invasive brain stimulation techniques. Experts are optimistic that this could pave the way for more effective mental health treatments in the future.',
+          },
+          {
+            event_classification: 'assault',
+            sentiment: 'negative',
+            platform: 'The Guardian',
+            title: 'Rise in Assault Cases Leads to Calls for Stricter Laws',
+            body: 'Following a recent surge in assault cases, citizens are demanding stricter laws. Critics argue that the current legal framework does not do enough to deter violent offenders. Meanwhile, lawmakers are deliberating over potential amendments to current legislation. Victim support groups are urging for swift and decisive action.',
+          },
+          {
+            event_classification: 'natural disaster',
+            sentiment: 'negative',
+            platform: 'Reuters',
+            title: 'Earthquake Shakes Major City, Destruction and Panic Ensue',
+            body: 'A 7.8 magnitude earthquake hit the city early this morning, causing widespread destruction. Buildings have crumbled, and several fires have broken out. Rescue teams are working tirelessly to locate and help survivors. The city is in a state of emergency, and the full extent of the damage is yet unknown.',
+          },
+          {
+            event_classification: 'mass casualty',
+            sentiment: 'negative',
+            platform: 'Al Jazeera',
+            title: 'Bombing in Market Place Claims Dozens of Lives',
+            body: 'An explosion in a bustling marketplace has resulted in mass casualties. The detonation occurred during peak hours, causing horrific damage. Emergency services are struggling to attend to the wounded. The authorities suspect this to be an act of terrorism and have initiated an investigation.',
+          },
+          {
+            event_classification: 'assault',
+            sentiment: 'negative',
+            platform: 'Fox News',
+            title: 'Increase in Street Violence Prompts Safety Concerns',
+            body: 'A recent uptick in street violence has residents living in fear. The police are increasing patrols, especially during the night. Community leaders are calling for more long-term solutions, like increased funding for social programs. The ongoing situation has sparked a heated debate about public safety in the city.',
+          },
+          {
+            event_classification: 'other',
+            sentiment: 'positive',
+            platform: 'The Globe and Mail',
+            title: 'New Research Brings Hope to Alzheimer’s Patients',
+            body: 'A groundbreaking study offers new hope for patients suffering from Alzheimer’s. The study indicates that early detection and a new line of treatment can slow the disease’s progression. The research is still in its early stages but has shown promising results. Patients and caregivers worldwide have welcomed this news with cautious optimism.',
+          },
+          {
+            event_classification: 'homicide',
+            sentiment: 'negative',
+            platform: 'ABC News',
+            title: 'Serial Killer on the Loose, Police Urge Public Caution',
+            body: 'A series of homicides have police suspecting a serial killer is at large. The local police department has issued a warning, asking residents to take extra precautions. There is currently a city-wide curfew in place until further notice. The investigation is ongoing, and updates will follow as more information becomes available.',
+          },
+        ],
+        filteredArticles: [],
       }
     },
     components: {
       TitleSection,
     },
     methods: {
-      async getArticles() {
-        const params = { n: this.limit }
+      filterArticles() {
         if (this.event_classification) {
-          params.event_classification = this.event_classification
+          this.filteredArticles = this.articles.filter(
+            article =>
+              article.event_classification === this.event_classification,
+          )
+        } else {
+          this.filteredArticles = this.articles
         }
-        const response = await axios.get(
-          'http://ai.tadhealth.com:8000/articles/',
-          { params },
-        )
-        this.articles = response.data
       },
     },
     watch: {
-      limit: 'getArticles',
-      event_classification: 'getArticles',
+      event_classification: 'filterArticles',
     },
-
     mounted() {
-      this.getArticles()
-      window.onload = function () {
-        $(function () {
-          if (window.location.protocol === 'https:')
-            window.location.protocol = 'http'
-        })
-      }
+      this.filteredArticles = this.articles
     },
   }
 </script>
