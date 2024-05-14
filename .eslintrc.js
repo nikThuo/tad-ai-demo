97c983489e43d9028fa2b6eff50c2cece2d218c6
miguelcoria94 <miguelcoria94@gmail.com>
2024-05-14 02:24:57 -0500

    combined demo

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,8 +26,6 @@ module.exports = {
     },
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 
     semi: ['error', 'never'],
     'max-len': 'off',
